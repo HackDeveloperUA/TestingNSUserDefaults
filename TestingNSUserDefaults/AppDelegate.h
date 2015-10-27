aca7ac9f7ae6b2b2d2cc0c64f9ecfa58b3044c59
HackDeveloperUA <hack@mac-mini-hack.local>
2015-10-27 14:59:41 +0300

    Initial Commit

---
+++ TestingNSUserDefaults/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestingNSUserDefaults
+//
+//  Created by Hack on 27.10.15.
+//  Copyright © 2015 ttconsalting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestingNSUserDefaults/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingNSUserDefaults
+//
+//  Created by Hack on 27.10.15.
+//  Copyright © 2015 ttconsalting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
